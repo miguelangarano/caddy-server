7fcf64ae11185707f97f22406d0ba26aea49c988
Miguel Langarano <miguelangarano@gmail.com>
2023-11-23 21:51:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Caddy server setup
+
+This project allows you to setup with just one script a Caddy server in a Ubuntu machine.
+
+
+# Pre requisites
+
+- An Ubuntu server machine with git
+
+
+# Setup
+
+1. Execute `cd /home`
+2. Execute `git clone https://github.com/miguelangarano/caddy-server.git`
+3. Execute `cd caddy-server`
+4. Execute `sudo su`
+5. Execute `chmod +x initial-setup.sh`
+
+
+# Usage
+
+You can edit the `Caddyfile` config but you must executue `docker-compose down` and then `docker-compose up -d`
+
+Here's an example for the Caddyfile setup
+
+```
+test.google.com {
+    reverse_proxy app:3000 {
+      flush_interval -1
+    }
+}
+```
---
+++ initial-setup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+### Initial setup
+
+# Update existing list of packages
+sudo apt-get update
+
+# Install prerequisite packages which let apt use packages over HTTPS
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+
+# Add Docker repository to APT sources
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the package database with Docker packages from the newly added repo
+sudo apt-get update
+
+# Open ports for caddy
+sudo ufw allow 80
+sudo ufw allow 443
+
+
+# Install Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+
+# Inform the user that a reboot or relogin is required
+echo "Docker installed successfully!"
+echo "You will need to reboot or log out & log back in for the group changes to take effect!"
+
+
+# Enable and start Docker service
+sudo systemctl enable docker
+sudo systemctl start docker
+
+
+# Display Docker version
+docker --version
+
+
+# Add docker compose repo
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
+# Set permissions for docker compose 
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+# Display docker compose version
+docker-compose --version
+
+
+echo "Docker compose installed successfully"
+
+# Create docker shared network across docker compose files
+docker network create webnet
+
+echo "Docker network: webnet - created successfully"
+
+# Create caddy volume
+sudo docker volume create caddy_data
+
+echo "Caddy docker volume created successfully"
+
+# Create caddy_config and Caddyfile
+mkdir caddy_config
+ln -s Caddyfile /caddy_config/Caddyfile
+
+echo "Created Caddyfile"
+
+
+# Start caddy
+docker-compose up -d
+
+echo "Caddy server started"
